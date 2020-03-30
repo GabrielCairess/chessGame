681435181d7ef1e96092c42d3baa2d45b0f9d454
gabrielcairess <gbrlcaires@gmail.com>
2020-03-30 20:34:45 -0300

    created position class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ChessGame.board;
 
 namespace ChessGame
 {
---
+++ board/Position.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.board
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position()
+        {
+        }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line.ToString()
+                + ", "
+                + Column();
+        }
+    }
+}
